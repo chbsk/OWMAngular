fbc67d24f232b909404f533c67e5f32875f991f6
Dula <m.ashf.abdull@gmail.com>
2019-12-14 13:13:36 -0700

    Reset everything cos the app broke beyond fixing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestButtonComponent } from './test-button/test-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TestButtonComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
