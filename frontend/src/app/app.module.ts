5accecf8f05cae657c8843cdf9038c5912ab839f
Dula <m.ashf.abdull@gmail.com>
2019-12-14 21:24:14 -0700

    working on login authentication with git

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule } from 'apollo-angular';
+import { HttpLinkModule } from 'apollo-angular-link-http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
